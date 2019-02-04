c4f7375d5b1daedbfdd43088a6830b130ad46499
Drazen Manojlovic <34537481+SpeedyBGD@users.noreply.github.com>
2019-02-04 23:54:44 +0100

    Add test for Patch todos id

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ app.patch('/todos/:id', (req,res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {$new: true}).then((todo) => {
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -140,6 +142,44 @@ describe('DELETE /todos/:id', () => {
         .expect(404)
         .end(done)
     });
+});
 
+describe('PATCH /todos/:id', () => {
+    it('Should update todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This sould be the new text';
 
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: true,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            expect(typeof res.body.todo.completedAt).toBe('number');
+        })
+        .end(done);
+    });
+    it('Should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'This sould be the new text!!!';
+
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: false,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toBeFalsy();
+        })
+        .end(done);
+
+    });
 });
